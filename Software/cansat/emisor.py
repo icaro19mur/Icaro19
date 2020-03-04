6b32e6df4087f385bcb914f8e2baef58548f6d78
icaro19mur <60033107+icaro19mur@users.noreply.github.com>
2020-03-04 21:28:00 +0100

    Add files via upload

---
+++ Software/cansat/emisor.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import RPi.GPIO as GPIO
+import serial
+import time
+import sys
+
+	
+	
+# SENSOR SMP280 - temperatura y presión
+from bmp280 import BMP280
+
+#SENSOR LSM303D  - acelerómetro
+from lsm303d import LSM303D
+	
+	
+	
+M0 = 22
+M1 = 27
+#MODE = ["BROADCAST_AND_MONITOR","P2P"]
+
+CFG_REG = [b'\xC2\x00\x09\xFF\xFF\x00\x62\x00\x17\x03\x00\x00',
+		   b'\xC2\x00\x09\x00\x00\x00\x62\x00\x17\x03\x00\x00']
+RET_REG = [b'\xC1\x00\x09\xFF\xFF\x00\x62\x00\x17\x03\x00\x00',
+		   b'\xC1\x00\x09\x00\x00\x00\x62\x00\x17\x03\x00\x00']
+r_buff = ""
+delay_temp = 1
+
+	
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(M0,GPIO.OUT)
+GPIO.setup(M1,GPIO.OUT)
+
+GPIO.output(M0,GPIO.LOW)
+GPIO.output(M1,GPIO.HIGH)
+time.sleep(0.01)
+
+ser = serial.Serial("/dev/ttyS0",9600)
+ser.flushInput()
+
+#Inicializar el BMP280
+try:
+	from smbus2 import SMBus
+except:
+	from smbus import SMBus
+
+bus = SMBus(1)
+bmp280 = BMP280(i2c_dev=bus)
+
+
+baseline_values = []
+baseline_size = 100
+
+print("inicializando linea base...")
+for i in range(baseline_size):
+	pressure = bmp280.get_pressure()
+	baseline_values.append(pressure)
+	time.sleep(1)
+	print(i,"% calculando linea base") 
+baseline = sum(baseline_values[:-25]) / len(baseline_values[:-25])
+
+#Inicializar el LSM303D
+lsm = LSM303D(0x1d)
+
+
+try :
+
+	if ser.isOpen() :
+		print("It's setting P2P mode")
+		ser.write(CFG_REG[1])
+		contador = 0
+	while True :
+				
+		
+		
+		if ser.inWaiting() > 0 :
+			time.sleep(0.1)
+			r_buff = ser.read(ser.inWaiting())
+			if r_buff == RET_REG[1] :
+				print("P2P modo activado")
+				GPIO.output(M1,GPIO.LOW)
+				time.sleep(0.01)
+				r_buff = ""
+			if r_buff != "" :
+				print("receive a P2P message:")
+				print(r_buff)
+				r_buff = ""
+		delay_temp += 1
+				
+		if delay_temp > 10000 :
+			# LEER DATOS
+			temperature = bmp280.get_temperature()
+			pressure = bmp280.get_pressure()
+			altitude = bmp280.get_altitude(qnh=baseline)
+			xyz = lsm.accelerometer()
+			accel = "{:06.2f}:{:06.2f}:{:06.2f}".format(*xyz)
+			
+			
+			#mensaje = str(contador) +" | Temperatura: "+str(round(temperature,2))+" | Presion: "+str(round(pressure,2))+"\r\n"
+			mensaje = str(contador)+ "," + str(round(temperature,2))+","+str(round(pressure,2))+","+ str(round(altitude,2))+","+ accel +"\r\n"
+			print(mensaje)
+			
+			ser.write(mensaje.encode())
+			delay_temp = 0
+			contador += 1
+except :
+	if ser.isOpen() :
+		ser.close()
+	GPIO.cleanup()
